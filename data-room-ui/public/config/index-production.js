1606db56ac04d336a22bd0e5393da0d53c753c05
hezhuozhuo <421045249@qq.com>
2025-03-06 16:35:15 +0800

    Fix:修改前端配置

--- data-room-ui/public/config/index-production.js
+++ data-room-ui/public/config/index-production.js
@@ -1,7 +1,7 @@
 window.ENV = 'production'
 var productionConfig = {
-  baseURL: 'http://127.0.0.1:9081/bigScreenServer',
-  fileUrlPrefix: 'http://127.0.0.1:9081/bigScreenServer' + '/static'
+  baseURL: 'http://47.104.235.67:9081/bigScreenServer',
+  fileUrlPrefix: 'http://47.104.235.67:9081/bigScreenServer' + '/static'
 }
 // 必须的
 window.CONFIG = {}
