d310343b99845fbb7705aeee2ee4fd791898dff6
hezhuozhuo <421045249@qq.com>
2025-03-20 16:54:37 +0800

    Fix：修改接口增加轮询

--- DataRoom/dataroom-core/src/main/java/com/gccloud/dataroom/core/module/chart/components/datasource/BaseChartDataSource.java
+++ DataRoom/dataroom-core/src/main/java/com/gccloud/dataroom/core/module/chart/components/datasource/BaseChartDataSource.java
@@ -1,6 +1,8 @@
 package com.gccloud.dataroom.core.module.chart.components.datasource;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -13,5 +15,13 @@ import lombok.Data;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
 public abstract class BaseChartDataSource {
 
+    @ApiModelProperty(notes="是否轮询")
+    private Boolean polling;
+
+    @ApiModelProperty(notes = "轮询间隔(毫秒)")
+    private String pollingInterval;
+    
+    @ApiModelProperty(notes = "定时器Id")
+    private String pollTimer;
 
 }
