9f781768add878586d3f0b1064e906d27fe1b22f
Wrong <1070750088@qq.com>
2025-04-14 18:25:15 +0800

    更新开发和生产环境配置，修改基础URL和文件前缀为新的服务器地址；优化桥梁监测页面样式，提升用户体验和可读性，增加状态颜色指示功能。

--- data-room-ui/public/config/index-development.js
+++ data-room-ui/public/config/index-development.js
@@ -1,7 +1,7 @@
 window.ENV = 'development'
 var developmentConfig = {
-  baseURL: 'http://47.104.235.67:9083/bigScreenServer',
-  fileUrlPrefix: 'http://47.104.235.67:9083/bigScreenServer' + '/static'
+  baseURL: 'http://47.115.210.16:9083/bigScreenServer',
+  fileUrlPrefix: 'http://47.115.210.16:9083/bigScreenServer' + '/static'
 }
 // 必须的
 window.CONFIG={}
