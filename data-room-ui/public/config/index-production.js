d05a4ca6ecf521050876674fa2bfb1820a54e302
Wrong <1070750088@qq.com>
2025-06-24 15:43:19 +0800

    fix: 添加iotBaseURL配置到生产环境
    为生产环境配置添加物联网服务的API基础地址，确保能正确连接到物联网平台

--- data-room-ui/public/config/index-production.js
+++ data-room-ui/public/config/index-production.js
@@ -1,7 +1,8 @@
 window.ENV = 'production'
 var productionConfig = {
   baseURL: '/bigScreenServer',
-  fileUrlPrefix: '/bigScreenServer' + '/static'
+  fileUrlPrefix: '/bigScreenServer' + '/static',
+  iotBaseURL: 'http://47.115.210.16:9999/api/v1',
 }
 // 必须的
 window.CONFIG = {}
